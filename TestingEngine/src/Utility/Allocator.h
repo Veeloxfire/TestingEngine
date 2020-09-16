fa902138976f84d77089877c06fbb12817490d09
Tom Hickson <thomasjhickson@gmail.com>
2020-09-16 16:02:55 +0100

    Allocator Functions
    To be used by Optional

---
+++ TestingEngine/src/Utility/Allocator.h
@@ -0,0 +1,19 @@
+#pragma once
+namespace Testing
+{
+	namespace Allocator
+	{
+		namespace Construct
+		{
+			template<typename Type, typename ... Args>
+			constexpr static void InPlace(Type& Obj, Args&& ... args) {
+				new(&Obj) Type{ std::forward<Args>(args)... };
+			}
+		}
+
+		template<typename Type>
+		constexpr static void Destruct(Type& Place) {
+			Place.~Type();
+		}
+	}
+}
